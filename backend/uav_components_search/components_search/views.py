056690dab4a61a903d6c061cf8f1c0f760691f4c
Olegurchick <64197137+Oleg-tech@users.noreply.github.com>
2024-04-22 09:31:22 +0300

    pagination on backend added

--- backend/uav_components_search/components_search/views.py
+++ backend/uav_components_search/components_search/views.py
@@ -3,19 +3,30 @@ from django.shortcuts import render
 from django.contrib import messages
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, serializers
 from django.http import HttpResponse
 
 from .data_parser.main_parser import main_parser
 from .utils import get_user_ip
 from .services import get_redis_connection
+from .api.pagination import ComponentsResultPagination
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
+class ComponentSerializer(serializers.Serializer):
+    componentName = serializers.CharField()
+    componentPrice = serializers.CharField()
+    componentImageURL = serializers.URLField()
+    componentExternalURL = serializers.URLField()
+    componentShopName = serializers.CharField()
+
+
 class FindComponentView(APIView):
+    pagination_class = ComponentsResultPagination
+
     def post(self, request, *args, **kwargs):
         query = request.data.get('query')
         print("Query = ", query)
@@ -23,16 +34,22 @@ class FindComponentView(APIView):
         if not query:
             return Response({'detail': 'Введіть товар для пошуку'}, status=status.HTTP_400_BAD_REQUEST)
 
+        paginator = self.pagination_class()
+
         user_ip = get_user_ip(request)
         redis_client = get_redis_connection()
 
         # Перевірка наявності такого запиту протягом останніх 10 хвилин
         query_recent_request = redis_client.get(query.strip())
         if query_recent_request:
-            return Response({
-                'detail': 'Query processed successfully',
-                'search_result': json.loads(query_recent_request)
-            })
+            components = json.loads(query_recent_request)
+            print("Number of components = ", len(components))
+            result_page = paginator.paginate_queryset(components, request)
+            serializer = ComponentSerializer(result_page, many=True)
+
+            return paginator.get_paginated_response(
+                serializer.data
+            )
 
         # Перевірка користувача на велику кількість запитів
         user_recent_request = redis_client.get(user_ip)
@@ -43,7 +60,11 @@ class FindComponentView(APIView):
 
         components = main_parser(user_ip=user_ip, query=query)
 
-        return Response({
+        # Пагінація
+        result_page = paginator.paginate_queryset(components, request)
+        serializer = ComponentSerializer(result_page, many=True)
+
+        return paginator.get_paginated_response({
             'detail': 'Query processed successfully',
-            'search_result': components
+            'search_result': serializer.data
         })
