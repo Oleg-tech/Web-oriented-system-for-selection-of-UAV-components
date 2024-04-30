3373deb7197a5165f6f5631932a92f9fd77c4234
Olegurchick <64197137+Oleg-tech@users.noreply.github.com>
2024-04-30 11:28:39 +0300

    components verification added

---
+++ backend/uav_components_search/components_search/data_parser/remove_inappropriate.py
@@ -0,0 +1,26 @@
+
+
+def check_component(component, key_words):
+    for key_word in key_words:
+        if key_word not in component['componentName'].lower():
+            return False
+    return True
+
+
+def remove_inappropriate_components(query, components) -> list:
+    verified_components = []
+
+    key_words = [word.strip() for word in query.split(' ') if len(word) > 3]
+
+    print("key words = ", key_words)
+
+    for component in components:
+        check_result = check_component(component=component, key_words=key_words)
+        if check_result:
+            verified_components.append(component)
+
+    print(verified_components)
+
+    return verified_components
+
+
